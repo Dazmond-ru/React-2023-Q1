2e9917034bebe49321e654f3f5a8e4374f28acc7
Sergey <fserega2014@yandex.ru>
2023-04-07 20:34:08 +0300

    feat: add preloader

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,31 +3,45 @@ import React, { useEffect, useState } from 'react'
 import ResultList from '../../components/ResultList/ResultList'
 import { fetchData } from '../../api/api'
 
+import preloader from '../../assets/preloader.gif'
+
+import styles from './Home.module.scss'
+
 const Home = () => {
   const [cardData, setCardData] = useState([])
   const [errorMessage, setErrorMessage] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetchData().then((data) => setCardData(data))
   }, [])
 
   const handleSearch = (searchValue: string) => {
+    setIsLoading(true)
     fetchData(searchValue)
       .then((data) => {
         setCardData(data)
         setErrorMessage('')
+        setIsLoading(false)
       })
       .catch((error) => {
         console.error(error)
         setCardData([])
         setErrorMessage('❌ No matches found')
+        setIsLoading(false)
       })
   }
 
   return (
     <div data-testid="home">
       <SearchBar onSearch={handleSearch} />
-      <ResultList cardData={cardData} errorMessage={errorMessage} />
+      {isLoading ? (
+        <div className={styles['home-preloader']}>
+          <img src={preloader} alt="preloader" />
+        </div>
+      ) : (
+        <ResultList cardData={cardData} errorMessage={errorMessage} />
+      )}
     </div>
   )
 }
